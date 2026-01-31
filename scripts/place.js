2ea6c67c6f0482be086515512d2379a1d3512532
BrunoCamara-BR <bsantos4@byupathway.edu>
2026-01-31 17:53:39 -0300

    w3 assigment fix 1

--- scripts/place.js
+++ scripts/place.js
@@ -1,3 +1,34 @@
+let temperature = document.querySelector("#temperature");
+let winds = document.querySelector("#winds");
+let windsChill = document.querySelector("#wind-chill");
+
+const calculateWindChill = (temperature, winds) => {
+  if (temperature <= 10 && winds > 3) {
+    let windsC =
+      13.12 +
+      0.6215 * temperature -
+      11.37 * winds ** 0.16 +
+      0.3965 * temperature * winds ** 0.16;
+
+    return windsC;
+  } else {
+    return "N/A";
+  }
+};
+
+console.log(temperature.value)
+console.log(temperature)
+console.log(4444444)
+
+let wchill = calculateWindChill(temperature.textContent, winds.textContent);
+
+if (wchill === "N/A") {
+    windsChill.textContent = wchill;
+} else {
+    windsChill.textContent = wchill.toFixed(2);
+};
+
+// time
 const today = new Date();
 const yearSpan = document.querySelector("#currentyear");
 
